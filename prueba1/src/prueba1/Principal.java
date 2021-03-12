c07085f76e1a5ed4a1c20e96a72e480ee7ae09e8
guillermo ruiz <william01es@hotmail.com>
2021-03-12 14:51:27 +0100

    commit and push tag prueba1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola mundo java-git2");
+		System.out.println("Hola mundo java-git2");
 		
 	}
 }
